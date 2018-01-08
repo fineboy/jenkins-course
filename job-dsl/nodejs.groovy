0f07c39bde205fb8d04a50ac7ea554d6c340e2f1
Innocent Anigbo <innocent.anigbo@yahoo.com>
2018-01-08 00:51:11 +0000

    Update nodejs.groovy

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,4 +1,4 @@
-job('NodeJS example') {
+job('NodeJS example from dsl') {
     scm {
         git('git://github.com/fineboy/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
